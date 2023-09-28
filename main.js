36645f67cc26aa7cc96d2913cd05bca6af095257
ChrisBenson <cbenson.prsvr@gmail.com>
2023-09-28 14:29:07 -0500

    done

--- main.js
+++ main.js
@@ -3,10 +3,49 @@
  ******************************/
 
 
+function isItLong(str) {
+  let int =(str.length)
+  if (int>20)
+  return "That's a long string!"  ;
+
+  else return }
+
+  function isItMedium(str) {
+    let num =(str.length)
+    if (num>=10 && num<=20)
+   
+    return "That's a regular sized string!" ;
+  
+    else return }
+
+    function isItShort(str){
+      let num=(str.length)
+      if (num<10)
+      return "That's a small string!"  ;
+    
+      else return "That's not a small string!"
+    }
 
 
+    function howLongIsMyString(str){
+      let num=(str.length)
+      if (num>20){
+      return "That's a long string!"}
+    else if  ((num >= 10) && (20 >= num))
+     {return "That's a regular sized string!"
+      }
+    else if (num<10){
+    return "That's a small string!"
+    }}
 
 
+function instructorHeight(str) {
+if (str==="Kyle"){return 70}
+else if (str==="Jason"){
+  return 73}
+  else {return "I don't know that instructor!"}}
+
+  
 /**********************************
  *      OUR CODE GOES BELOW.      *
  * THIS IS FOR INTERNAL USE ONLY. *
